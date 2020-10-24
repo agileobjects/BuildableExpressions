78339cf371283fc6efbc157c0c1f68c396ab24a5
Steve Wilkes <steve@agileobjects.co.uk>
2020-10-24 16:04:04 +0100

    Moving interfaces to dedicated files

---
+++ BuildableExpressions/SourceCode/Api/IGenericParameterConfigurator.cs
@@ -0,0 +1,22 @@
+﻿namespace AgileObjects.BuildableExpressions.SourceCode.Api
+{
+    using System;
+
+    /// <summary>
+    /// Provides options to configure a <see cref="TypeExpression"/> or <see cref="MethodExpression"/>'s
+    /// generic parameters.
+    /// </summary>
+    public interface IGenericParameterConfigurator
+    {
+        /// <summary>
+        /// Adds a <see cref="GenericParameterExpression"/> to the <see cref="TypeExpression"/> or
+        /// <see cref="MethodExpression"/>.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="GenericParameterExpression"/>.</param>
+        /// <param name="configuration">The configuration to use for the <see cref="GenericParameterExpression"/>.</param>
+        /// <returns>The newly-created <see cref="GenericParameterExpression"/>.</returns>
+        GenericParameterExpression AddGenericParameter(
+            string name,
+            Action<IGenericParameterExpressionConfigurator> configuration);
+    }
+}
--- BuildableExpressions/SourceCode/Api/IMethodExpressionConfigurator.cs
+++ BuildableExpressions/SourceCode/Api/IMethodExpressionConfigurator.cs
@@ -5,24 +5,6 @@
     using ReadableExpressions;
 
     /// <summary>
-    /// Provides options to configure a <see cref="TypeExpression"/> or <see cref="MethodExpression"/>'s
-    /// generic parameters.
-    /// </summary>
-    public interface IGenericParameterConfigurator
-    {
-        /// <summary>
-        /// Adds a <see cref="GenericParameterExpression"/> to the <see cref="TypeExpression"/> or
-        /// <see cref="MethodExpression"/>.
-        /// </summary>
-        /// <param name="name">The name of the <see cref="GenericParameterExpression"/>.</param>
-        /// <param name="configuration">The configuration to use for the <see cref="GenericParameterExpression"/>.</param>
-        /// <returns>The newly-created <see cref="GenericParameterExpression"/>.</returns>
-        GenericParameterExpression AddGenericParameter(
-            string name,
-            Action<IGenericParameterExpressionConfigurator> configuration);
-    }
-
-    /// <summary>
     /// Provides options to configure a <see cref="MethodExpression"/>.
     /// </summary>
     public interface IMethodExpressionConfigurator : IGenericParameterConfigurator
