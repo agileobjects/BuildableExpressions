95a09f441606598eb24089066ce75f0f72f063b5
Steve Wilkes <steve@agileobjects.co.uk>
2020-11-08 15:57:06 +0000

    Organising tests

--- BuildableExpressions.UnitTests/WhenBuildingMethodsIncorrectly.cs
+++ BuildableExpressions.UnitTests/WhenBuildingMethodsIncorrectly.cs
@@ -8,6 +8,148 @@
     public class WhenBuildingMethodsIncorrectly
     {
         [Fact]
+        public void ShouldErrorIfNullMethodName()
+        {
+            var methodNameEx = Should.Throw<ArgumentException>(() =>
+            {
+                BuildableExpression.SourceCode(sc => sc
+                    .AddClass(cls => cls
+                        .AddMethod(default(string))));
+            });
+
+            methodNameEx.Message.ShouldContain("cannot be null");
+        }
+
+        [Fact]
+        public void ShouldErrorIfWhitespaceMethodName()
+        {
+            var methodNameEx = Should.Throw<ArgumentException>(() =>
+            {
+                BuildableExpression.SourceCode(sc => sc
+                    .AddClass(cls => cls
+                        .AddMethod("\t")));
+            });
+
+            methodNameEx.Message.ShouldContain("cannot be blank");
+        }
+
+        [Fact]
+        public void ShouldErrorIfInvalidMethodName()
+        {
+            var methodNameEx = Should.Throw<ArgumentException>(() =>
+            {
+                BuildableExpression.SourceCode(sc => sc
+                    .AddStruct(cls => cls
+                        .AddMethod(" My_Method")));
+            });
+
+            methodNameEx.Message.ShouldContain("invalid method name");
+        }
+
+        [Fact]
+        public void ShouldErrorIfNullGenericMethodParameterName()
+        {
+            var paramNameEx = Should.Throw<ArgumentException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddClass(cls =>
+                    {
+                        cls.AddMethod("NullParam", m =>
+                        {
+                            m.AddGenericParameter(null);
+                        });
+                    });
+                });
+            });
+
+            paramNameEx.Message.ShouldContain("cannot be null");
+        }
+
+        [Fact]
+        public void ShouldErrorIfBlankGenericMethodParameterName()
+        {
+            var paramNameEx = Should.Throw<ArgumentException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddClass(cls =>
+                    {
+                        cls.AddMethod("BlankParam", m =>
+                        {
+                            m.AddGenericParameter(string.Empty);
+                        });
+                    });
+                });
+            });
+
+            paramNameEx.Message.ShouldContain("cannot be blank");
+        }
+
+        [Fact]
+        public void ShouldErrorIfWhitespaceGenericMethodParameterName()
+        {
+            var paramNameEx = Should.Throw<ArgumentException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddClass(cls =>
+                    {
+                        cls.AddMethod("WhitespaceParam", m =>
+                        {
+                            m.AddGenericParameter("    ");
+                        });
+                    });
+                });
+            });
+
+            paramNameEx.Message.ShouldContain("cannot be blank");
+        }
+
+        [Fact]
+        public void ShouldErrorIfInvalidMethodGenericParameterName()
+        {
+            var paramNameEx = Should.Throw<ArgumentException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddClass(cls =>
+                    {
+                        cls.AddMethod("InvalidParam", m =>
+                        {
+                            m.AddGenericParameter("Param1!");
+                        });
+                    });
+                });
+            });
+
+            paramNameEx.Message.ShouldContain("invalid generic parameter name");
+        }
+
+        [Fact]
+        public void ShouldErrorIfDuplicateMethodGenericParameterNames()
+        {
+            var configEx = Should.Throw<InvalidOperationException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddClass("Class1", cls =>
+                    {
+                        cls.AddMethod("Method1", m =>
+                        {
+                            m.AddGenericParameter("T1");
+                            m.AddGenericParameter("T1");
+                        });
+                    });
+                });
+            });
+
+            configEx.Message.ShouldContain("Class1.Method1");
+            configEx.Message.ShouldContain("duplicate generic parameter name");
+            configEx.Message.ShouldContain("T1");
+        }
+
+        [Fact]
         public void ShouldErrorIfAbstractMethodAddedToNonAbstractClass()
         {
             var methodEx = Should.Throw<InvalidOperationException>(() =>
@@ -78,6 +220,74 @@
 
             methodEx.Message.ShouldContain("'void MyMethod(DateTime date, long number)'");
             methodEx.Message.ShouldContain("both static and abstract");
+        }
+
+        [Fact]
+        public void ShouldErrorIfDuplicateStructMethodSignature()
+        {
+            var methodEx = Should.Throw<InvalidOperationException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddStruct("MyStruct", str =>
+                    {
+                        str.AddMethod("DoNothing", m =>
+                        {
+                            m.AddParameter(Parameter(typeof(long), "number"));
+                            m.SetBody(Default(typeof(void)));
+                        });
+
+                        str.AddMethod("DoNothing", m =>
+                        {
+                            m.AddParameter(Parameter(typeof(long), "number"));
+                            m.SetBody(Default(typeof(void)));
+                        });
+                    });
+                });
+            });
+
+            methodEx.Message.ShouldContain("MyStruct");
+            methodEx.Message.ShouldContain("duplicate method signature");
+            methodEx.Message.ShouldContain("'void DoNothing(long number)'");
+        }
+
+        [Fact]
+        public void ShouldErrorIfStructMethodBodyNotSet()
+        {
+            var methodEx = Should.Throw<InvalidOperationException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddStruct("EmptyMethodStruct", str =>
+                    {
+                        str.AddMethod("EmptyMethod", m => { });
+                    });
+                });
+            });
+
+            methodEx.Message.ShouldContain("void EmptyMethodStruct.EmptyMethod()");
+            methodEx.Message.ShouldContain("no method body defined");
+        }
+
+        [Fact]
+        public void ShouldErrorIfMethodAccessesUnscopedVariable()
+        {
+            var methodEx = Should.Throw<NotSupportedException>(() =>
+            {
+                BuildableExpression.SourceCode(sc =>
+                {
+                    sc.AddClass(cls =>
+                    {
+                        var int1Variable = Parameter(typeof(int), "int1");
+                        var int2Variable = Variable(typeof(int), "int2");
+                        var addInts = Add(int1Variable, int2Variable);
+
+                        cls.AddMethod(addInts);
+                    });
+                });
+            });
+
+            methodEx.Message.ShouldContain("undefined variable(s) 'int int1', 'int int2'");
         }
     }
 }
