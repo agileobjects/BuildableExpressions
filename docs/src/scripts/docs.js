e8c50fcca616a1ff641c40d4b66a2565d4df2798
Steve Wilkes <steve@agileobjects.co.uk>
2021-06-26 11:39:50 +0100

    Dox

--- docs/src/scripts/docs.js
+++ docs/src/scripts/docs.js
@@ -1,19 +1,19 @@
 $(function () {
-    var hlCode = document.querySelectorAll('pre code.cs'),
+    var hlCode = document.querySelectorAll('pre code.language-cs'),
         i, l,
         hlLength = hlCode.length,
-        mapperRegex = /\bBuildableExpression\b/g,
+        buildExpRegex = /\bBuildableExpression\b/g,
         typeRegex = /(new<\/span>\W+|class<\/span> <span class="hljs-title">|public<\/span>\W+|: <span class="hljs-title">|&lt;)([A-Z][^& \(\[\]]+)( |{|\(|\[\]&gt;|&gt;)/g,
         genericTypeRegex = /(I{0,1}Dictionary|IEnumerable|IReadOnlyCollection|I{0,1}Collection|I{0,1}List)&lt;/g,
         observer = new MutationObserver(function (mutations) {
             for (i = 0, l = mutations.length; i < l; ++i) {
                 var mutation = mutations[i];
                 if (mutation.attributeName === 'class') {
-                    var innerHTML = mutation.target.innerHTML
-                        .replace(mapperRegex, '<span class="hljs-type">BuildableExpression</span>')
+                    var innerHtml = mutation.target.innerHTML
+                        .replace(buildExpRegex, '<span class="hljs-type">BuildableExpression</span>')
                         .replace(typeRegex, '$1<span class="hljs-type">$2</span>$3')
                         .replace(genericTypeRegex, '<span class="hljs-type">$1</span>&lt;');
-                    mutation.target.innerHTML = innerHTML;
+                    mutation.target.innerHTML = innerHtml;
                 }
             }
         }),
